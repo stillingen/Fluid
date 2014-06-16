0d2a56e2a677a44bb7a8e7d6d3a439ef81952510
Håkon Stillingen <haakon@stillingen.com>
2014-06-16 07:48:42 +0200

    adding full width widgets on front page

--- functions.php
+++ functions.php
@@ -22,5 +22,51 @@ add_theme_support( 'genesis-responsive-viewport' );
 //* Add support for custom background
 add_theme_support( 'custom-background' );
 
+
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
+
+//* Register Home Slider widget area
+genesis_register_sidebar( array(
+	'id'			=> 'home-slider',
+	'name'			=> 'Home Slider',
+	'description'	=> 'This is the home slider section'
+) );
+
+
+add_action( 'genesis_after_header', 'sk_home_featured' );
+/**
+ * Display Home Slider widget area's contents below Navigation on homepage.
+ *
+ * @author Sridhar Katakam
+ * @link   http://sridharkatakam.com/full-width-soliloquy-slider-genesis/
+ */
+function sk_home_featured() {
+	if ( is_home() || is_front_page() ) {
+		genesis_widget_area( 'home-slider', array(
+			'before'	=> '<div class="home-slider widget-area">',
+			'after'		=> '</div>',
+		) );
+	}
+}
+
+//* Register widget areas
+genesis_register_sidebar( array(
+	'id'          => 'home-top',
+	'name'        => __( 'Home - Top', 'Fluid' ),
+	'description' => __( 'This is the top section of the Home page.', 'Fluid' ),
+) );
+
+genesis_register_sidebar( array(
+	'id'          => 'home-middle',
+	'name'        => __( 'Home - Middle', 'Fluid' ),
+	'description' => __( 'This is the middle section of the Home page.', 'Fluid' ),
+) );
+
+genesis_register_sidebar( array(
+	'id'          => 'home-bottom',
+	'name'        => __( 'Home - Bottom', 'Fluid' ),
+	'description' => __( 'This is the bottom section of the Home page.', 'Fluid' ),
+) );
+
+?>
