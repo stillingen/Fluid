756c719f2ac932843e9125217cc5b34e3bc946de
Håkon Stillingen <haakon@stillingen.com>
2014-06-18 16:59:52 +0200

    adding support for schema.org/itemslist, custom woocommerce file as part of template

---
+++ woocommerce/loop/loop-start.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Product Loop Start
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.0.0
+ */
+?>
+<span itemscope itemtype="http://schema.org/ItemList">
+<meta itemprop="mainContentOfPage" content="true"/>
+<ul class="products">
