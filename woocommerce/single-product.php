c49e058335f2e2ce9c6c7838493bae9d360666f5
Håkon Stillingen <haakon@stillingen.com>
2014-06-18 22:20:22 +0200

    Removing related products from Woocommerce single-product.php

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -81,5 +81,6 @@ function gencwooc_single_product_loop() {
 
 	do_action( 'woocommerce_after_main_content' );
 }
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
 
 genesis();
