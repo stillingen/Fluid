3c0ece767152c6a318eef0aae93ac34d9214bb15
Håkon Stillingen <haakon@stillingen.com>
2014-09-09 06:59:22 +0200

    Adding customer header to compensate for missing H1 definition in Genesis sample theme

--- functions.php
+++ functions.php
@@ -239,6 +239,28 @@ global $post;
     }
  
 }
+// Filter the title with a custom function
+add_filter('genesis_seo_title', 'wap_site_title' );
+
+// Add additional custom style to site header
+function wap_site_title( $title ) {
+
+    	// Change $custom_title text as you wish
+	$custom_title = '<span class="custom-title">Fluid</span>';
+
+	// Don't change the rest of this on down
+
+	// If we're on the front page or home page, use `h1` heading, otherwise us a `p` tag
+	$tag = ( is_home() || is_front_page() ) ? 'h1' : 'p';
+
+	// Compose link with title
+	$inside = sprintf( '<a href="%s" title="%s">%s</a>', trailingslashit( home_url() ), esc_attr( get_bloginfo( 'name' ) ), $custom_title );
+
+	// Wrap link and title in semantic markup
+	$title = sprintf ( '<%s class="site-title" itemprop="headline">%s</%s>', $tag, $inside, $tag );
+	return $title;
+}
+
 /**
  * Register and Enqueue Primary Navigation Menu Script
  * 
