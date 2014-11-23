2bef6602c9fc4ec9ce0adae0fa3cc58bcdaf1c73
Håkon Stillingen <haakon@stillingen.com>
2014-11-23 18:57:09 +0100

    Use WC 2.0 variable price format, now include sale price strikeout

--- functions.php
+++ functions.php
@@ -302,6 +302,24 @@ function wc_remove_related_products( $args ) {
 }
 add_filter('woocommerce_related_products_args','wc_remove_related_products', 10); 
 
+// Use WC 2.0 variable price format, now include sale price strikeout
+add_filter( 'woocommerce_variable_sale_price_html', 'wc_wc20_variation_price_format', 10, 2 );
+add_filter( 'woocommerce_variable_price_html', 'wc_wc20_variation_price_format', 10, 2 );
+function wc_wc20_variation_price_format( $price, $product ) {
+	// Main Price
+	$prices = array( $product->get_variation_price( 'min', true ), $product->get_variation_price( 'max', true ) );
+	$price = $prices[0] !== $prices[1] ? sprintf( __( 'From: %1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+	// Sale Price
+	$prices = array( $product->get_variation_regular_price( 'min', true ), $product->get_variation_regular_price( 'max', true ) );
+	sort( $prices );
+	$saleprice = $prices[0] !== $prices[1] ? sprintf( __( 'From: %1$s', 'woocommerce' ), wc_price( $prices[0] ) ) : wc_price( $prices[0] );
+
+	if ( $price !== $saleprice ) {
+		$price = '<del>' . $saleprice . '</del> <ins>' . $price . '</ins>';
+	}
+	return $price;
+}
+
 /******------------End Woocommerce config--------------------*****/
 
 ?>
